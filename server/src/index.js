42e8a4e722ef5cb18b2b7745b416572cda46e97c
swarup-prog <lswarup1234@gmail.com>
2023-07-25 07:49:17 +0545

    Upadated and added node

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("This is a get request!!");
+});
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000.");
 });

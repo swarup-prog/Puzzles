c519f2ffb756ba5545f0b0460edce77a6c4543d4
swarup-prog <lswarup1234@gmail.com>
2023-07-27 18:19:19 +0545

    Updated Product Details page

--- client/src/pages/productDetails.jsx
+++ client/src/pages/productDetails.jsx
@@ -1,14 +1,27 @@
 import { useParams } from "react-router-dom";
 
 import Footer from "../components/Footer";
+import NavigationButton from "../components/buttons/NavigationButton";
+
+import clothesData from "../data/clothesData";
 
 const ProductDetails = () => {
   const { productId } = useParams();
 
+  const cloth = clothesData.find((item) => item.id === productId);
+
   return (
     <div style={styles.productDetailContainer}>
-      <section style={styles.productDetail}>Procuct Id: {productId}</section>
-      <section style={styles.productDescReview}></section>
+      <section style={styles.productDetail}>
+        <img src={cloth.image} alt="cloth" width="550px" height="685px" />
+        <div style={styles.details}>
+          <div style={styles.prodName}>{cloth.name}</div>
+          <div style={styles.price}>$ {cloth.price}</div>
+          <div style={styles.clothDesc}>{cloth.description}</div>
+          <NavigationButton to="" name="ADD TO CART" />
+        </div>
+      </section>
+      <section style={styles.productDescReview}>dafadf</section>
       {/* Footer  */}
       <Footer />
     </div>
@@ -18,13 +31,24 @@ const ProductDetails = () => {
 export default ProductDetails;
 
 const styles = {
-  productDetailContainer: {},
+  productDetailContainer: {
+    display: "flex",
+    flexDirection: "column",
+  },
 
   productDetail: {
-    fontWeight: 700,
-    fontSize: "40px",
-    height: "70vh",
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "40px 165px 110px 165px",
   },
+
+  details: {},
+
+  prodName: {},
+
+  price: {},
+
+  clothDesc: {},
 
   productDescReview: {},
 };

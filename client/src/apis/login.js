ff11e54e7601ddcad8b56803b098cfc7dae5559e
swarup-prog <lswarup1234@gmail.com>
2023-08-11 13:09:52 +0545

    Added login API

---
+++ client/src/apis/login.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const signup = async ({ email, password }) => {
+  const API_URL = "http://localhost:8000/api";
+
+  try {
+    const response = await axios.post(`${API_URL}/login`, { email, password });
+
+    const data = response?.data;
+    return { success: true, data };
+  } catch (error) {
+    if (
+      error.response &&
+      error.response?.status >= 400 &&
+      error.response?.status <= 500
+    ) {
+      return {
+        success: false,
+        error: error.response.data.message || error.response.data,
+      };
+    }
+  }
+};
